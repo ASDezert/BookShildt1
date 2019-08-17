6f20def4f0ada9d5a60f39f1874297e12993b15c
Nikita <asdezert@gmail.com>
2019-08-18 00:30:45 +0300

    Shildt Prog1_2

--- Shildt1/src/Example/Prog1_2.java
+++ Shildt1/src/Example/Prog1_2.java
@@ -1,5 +1,17 @@
 package Example;
 
 public class Prog1_2 {
-    
+    public static void main(String[] args) {
+        int var1;
+        int var2;
+
+        var1 = 1024;
+
+        System.out.println("Переменная varl содержит " + var1);
+
+        var2 = var1 / 2;
+
+        System.out.print("Переменная var2 содержит varl / 2: ");
+        System.out.println(var2);
+    }
 }
