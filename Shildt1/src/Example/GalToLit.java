97092d5a7b5ec032a2eb5f2c6ba8f6915c1a9f01
Nikita <asdezert@gmail.com>
2019-08-18 19:28:01 +0300

    Shildt Prog

---
+++ Shildt1/src/Example/GalToLit.java
@@ -0,0 +1,9 @@
+package Example;
+
+public class GalToLit {
+    public static void main(String[] args) {
+        double gallons = 10;
+        double liters = gallons * 3.7854;
+        System.out.println(gallons + " галлонам соответствует " + liters + " литра");
+    }
+}
